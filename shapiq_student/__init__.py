3fe98781e6ad5069edafe2bf1e91b66c6509d999
heeso <heeso1747@gmail.com>
2025-07-17 23:09:30 +0200

    fix docs-error

--- shapiq_student/__init__.py
+++ shapiq_student/__init__.py
@@ -1,12 +1,9 @@
-"""Source code for the shapiq_student package.
+"""Source code for the shapiq_student package."""
 
-:no-index:
-"""
-
+from . import subset_finding
 from .basic_knn_explainer import KNNExplainer as BasicKNNExplainer
 from .gaussian_copula_imputer import GaussianCopulaImputer
 from .gaussian_imputer import GaussianImputer
-from .subset_finding import subset_finding
 from .weighted_knn_explainer import KNNExplainer as WeightedKNNExplainer
 
 __all__ = [
