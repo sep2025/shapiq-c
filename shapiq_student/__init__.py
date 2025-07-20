eacc8652d6e188a14885e184bc5c10e02210953d
heeso <heeso1747@gmail.com>
2025-07-20 19:36:12 +0200

    sort imports

--- shapiq_student/__init__.py
+++ shapiq_student/__init__.py
@@ -1,9 +1,9 @@
 """Source code for the shapiq_student package."""
 
 from .basic_knn_explainer import KNNExplainer as BasicKNNExplainer
+from .coalition_finding import subset_finding
 from .gaussian_copula_imputer import GaussianCopulaImputer
 from .gaussian_imputer import GaussianImputer
-from .coalition_finding import subset_finding
 from .weighted_knn_explainer import KNNExplainer as WeightedKNNExplainer
 
 __all__ = [
